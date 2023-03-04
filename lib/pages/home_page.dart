6285bc1e82a713c8bb4673c277610f4971827040
KLYCHub <klychub@gmail.com>
2023-03-04 14:51:21 +0300

    Update V1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flip_card/flip_card.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:audioplayers/audioplayers.dart';
 
 import '../product/color/color_items.dart';
 import '../product/lang/langue_item.dart';
