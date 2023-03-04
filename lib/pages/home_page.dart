e453b5c992c30ea5e398c06d6a465cfce93c770b
KLYCHub <klychub@gmail.com>
2023-03-04 20:48:49 +0300

    Update V1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import 'package:url_launcher/url_launcher.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../product/app_bar_button_link.dart';
 import '../product/color/color_items.dart';
 import '../product/counter.dart';
 import '../product/lang/langue_item.dart';
@@ -42,50 +41,26 @@ class _MyHomePageState extends State<MyHomePage> {
     return Text(
       ProjectTexts().appBarTitle,
       style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-          fontWeight: FontWeight.w700,
-          letterSpacing: ProjectNumbers().headsTailsLetterSpacing,
-          color: ProjectColor().whiteARGB),
+            fontWeight: FontWeight.w700,
+            letterSpacing: ProjectNumbers().headsTailsLetterSpacing,
+            color: ProjectColor().whiteARGB,
+          ),
     );
   }
 
-  //update
+  //----------------------------------------------------------------------------
+
   AppBar _appBar(BuildContext context) {
     return AppBar(
-      leading: IconButton(
-        icon: FaIcon(
-          FontAwesomeIcons.circleInfo,
-          size: ProjectNumbers().appBarActionsIcons,
-        ),
-        onPressed: () async {
-          final websiteUrl =
-              Uri.parse('https://www.instagram.com/yelbegensoftware/'); //update
-          if (await canLaunchUrl(websiteUrl)) {
-            launchUrl(websiteUrl);
-          } else {
-            if (kDebugMode) {
-              print("can't lahunch $websiteUrl"); //update
-            }
-          }
-        },
+      leading: AppBarButtonLink(
+        linkUrl: ProjectAssets().webLink,
+        icon: FontAwesomeIcons.circleInfo,
       ),
       actions: [
-        IconButton(
-          icon: FaIcon(
-            FontAwesomeIcons.googlePlay,
-            size: ProjectNumbers().appBarActionsIcons,
-          ),
-          onPressed: () async {
-            final websiteUrl = Uri.parse(
-                'https://www.instagram.com/yelbegensoftware/'); //update
-            if (await canLaunchUrl(websiteUrl)) {
-              launchUrl(websiteUrl);
-            } else {
-              if (kDebugMode) {
-                print("can't lahunch $websiteUrl"); //update
-              }
-            }
-          },
-        ),
+        AppBarButtonLink(
+          linkUrl: ProjectAssets().webLink,
+          icon: FontAwesomeIcons.googlePlay,
+        )
       ],
       centerTitle: true,
       title: _appBarTitle(context),
